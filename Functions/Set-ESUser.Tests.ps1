b1384f2f5b4c8170f275cabf669c8372fd5f8644
Nathan Stohlmann <nathan@virtualmicropolis.com>
2016-11-07 17:30:37 -0600

    New Function: Set-ESUser - Creates or updates a native user

---
+++ Functions/Set-ESUser.Tests.ps1
@@ -0,0 +1,147 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Set-ESUser" {
+	Mock -CommandName Invoke-RestMethod -MockWith { return $true }
+	
+	#store existing environment values
+	$existingBaseUri = $env:esBaseUri
+	$existingCredential = $esCredential
+	
+	$globalUri = "http://some.esserver.com:12345"
+	$localUri = "http://someother.esserver.com:23456"
+	$globalCreds = [pscredential]::new( "SomeUser" , ( ConvertTo-SecureString -String "SomePassword" -AsPlainText -Force ) )
+	$localCreds = [pscredential]::new( "SomeLocalUser" , ( ConvertTo-SecureString -String "SomeLocalPassword" -AsPlainText -Force ) )
+	
+	$usernameVal = "SomeUser"
+	$passwordVal = "SomePassword123"
+	$rolesSingleVal = "SomeRole"
+	$rolesMultiVal = @( "SomeOtherRole","YetAnotherRole" )
+	$fullnameVal = "Jane D. User"
+	$emailVal = "someuser@some.domain.com"
+	$metadataVal = @{ param1 = "Some value"; param2 = $false; paramList = @( 3..9 ) }
+	
+	$goodParams = @{ Username = $usernameVal; Password = $passwordVal }
+	
+    Context "Standard parameter tests" {
+		$env:esBaseUri = $null
+		$esCredential = $null
+		
+		It "throws if the BaseURI parameter and esBaseUri environment variables are null or not specified" {
+			$env:esBaseUri = $null
+			$esCredential = $globalCreds
+			{ Set-ESUser @goodParams } | Should Throw
+		}
+		
+		It "throws if the Credential parameter and esCredential global variables are null or not specified" {
+			<# NRS 11/7/2016 - Disabling this test until it can be tested accurately
+			$env:esBaseUri = $globalUri
+			$esCredential = $null
+			{ Set-ESUser @goodParams } | Should Throw
+			#>
+		}
+		
+		It "passes the value for the BaseURI parameter if specified" {
+			$env:esBaseUri = $globalUri
+			$esCredential = $globalCreds
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { $Uri -like "$localUri/*" } }
+			Mock @mockParams -MockWith { return $true }
+			Set-ESUser @goodParams -BaseURI $localUri
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+		
+		It "passes the credential object for the Credential object if specified" {
+			$env:esBaseUri = $globalUri
+			$esCredential = $globalCreds
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { $Credential -eq $localCreds } }
+			Mock @mockParams -MockWith { return $true }
+			Set-ESUser @goodParams -Credential $localCreds
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+		
+		It "passes the value for the global esBaseUri variable if the BaseURI parameter is not specified" {
+			$env:esBaseUri = $globalUri
+			$esCredential = $globalCreds
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { $Uri -like "$globalUri/*" } }
+			Mock @mockParams -MockWith { return $true }
+			Set-ESUser @goodParams
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+		
+		It "passes the credential object for the global esCredential variable if the Credential parameter is not specified" {
+			$env:esBaseUri = $globalUri
+			$esCredential = $globalCreds
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { $Credential -eq $globalCreds } }
+			Mock @mockParams -MockWith { return $true }
+			Set-ESUser @goodParams
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+	}
+	
+    Context "Parameter Tests" {
+		$env:esBaseUri = $globalUri
+		$esCredential = $globalCreds
+		
+		It "throws if the Username parameter is null or not specified" {
+			{ Set-ESUser -Password $passwordVal -Username } | Should Throw
+		}
+		
+		It "passes the value of the Username parameter in the uri" {
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { $Uri -like "*$Username" } }
+			Mock @mockParams -MockWith { return $true }
+			Set-ESUser @goodParams
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+		
+		It "throws if the Password parameter is null or not specified" {
+			{ Set-ESUser -Username $usernameVal -Password } | Should Throw
+		}
+		
+		It "passes the value of the Password parameter in the body of the request" {
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { ( $Body | ConvertFrom-JSON ).password -eq $passwordVal } }
+			Mock @mockParams -MockWith { return $true }
+			Set-ESUser @goodParams
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+		
+		It "passes a single value for the Roles parameter in the body of the request" {
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { ( $Body | ConvertFrom-JSON ).roles -eq $rolesSingleVal } }
+			Mock @mockParams -MockWith { return $true }
+			Set-ESUser @goodParams -Roles $rolesSingleVal
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+		
+		It "passes multiple values for the Roles parameter in the body of the request" {
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { -not ( Compare-Object -ReferenceObject ( $Body | ConvertFrom-JSON ).roles -DifferenceObject $rolesMultiVal ) } }
+			Mock @mockParams -MockWith { return $true }
+			Set-ESUser @goodParams -Roles $rolesMultiVal
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+		
+		It "passes the value of the FullName parameter in the body of the request" {
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { ( $Body | ConvertFrom-JSON ).'full_name' -eq $fullnameVal } }
+			Mock @mockParams -MockWith { return $true }
+			Set-ESUser @goodParams -FullName $fullnameVal
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+		
+		It "passes the value of the Email parameter in the body of the request" {
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { ( $Body | ConvertFrom-JSON ).email -eq $emailVal } }
+			Mock @mockParams -MockWith { return $true }
+			Set-ESUser @goodParams -Email $emailVal
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+		
+		It "passes the value of the Metadata parameter in the body of the request" {
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { -not ( Compare-Object -ReferenceObject ( $Body | ConvertFrom-JSON ).metadata -DifferenceObject (New-Object -TypeName psobject -Property $metadataVal ) ) } }
+			Mock @mockParams -MockWith { return $true }
+			Set-ESUser @goodParams -Metadata $metadataVal
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+	}
+	
+	#restore environment values
+	$env:esBaseUri = $existingBaseUri
+	$esCredential = $existingCredential
+}
---
+++ Functions/Set-ESUser.ps1
@@ -0,0 +1,78 @@
+﻿function Set-ESUser {
+	<#
+		.SYNOPSIS
+			Updates or creates a native user on an ElasticSearch cluster.
+		.DESCRIPTION
+			Updates or creates a native user on an ElasticSearch cluster and returns an object with the account information using the Shield plugin.
+			
+			If the "created" property of the returned object is True the account was created. If the property is False the account was updated.
+			
+			Wraps the POST method of the /_shield/user/<username> REST api.
+		.PARAMETER Username
+			The name of the user to create
+		.PARAMETER Password
+			The password to set for the new account
+		.PARAMETER Roles
+			One or more roles to set for the new account
+		.PARAMETER FullName
+			The display name for the account
+		.PARAMETER Email
+			An email address to associate with the account.
+		.PARAMETER Metadata
+			A hashtable of arbitrary custom settings for the user.
+		.PARAMETER BaseURI
+			The URI for an ElasticSearch server. If not specified uses the value of $env:esBaseURI
+		.PARAMETER Credential
+			User account for specified ElasticSearch server. If not specified users the value of $esCredential
+		.LINKS
+			https://www.elastic.co/guide/en/shield/current/shield-rest.html#shield-users-rest
+	#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Username,
+		
+		[Parameter(Mandatory=$true)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Password,
+		
+		[string[]]$Roles,
+		
+		[string]$FullName,
+		
+		[string]$Email,
+		
+		[hashtable]$Metadata,
+		
+		[ValidateNotNullOrEmpty()]
+		[string]$BaseURI = $env:esBaseURI,
+		
+		[ValidateScript({ $_ -ne $null })]
+		[pscredential]$Credential = $esCredential
+	)
+	Begin {
+		# Set the request URI
+		[uri]$uri = [uri]::new( [uri]$BaseURI , "/_shield/user/$Username" )
+		Write-Verbose "Request URI: $($uri.AbsoluteUri)"
+		
+		# create the request body
+		$bodyHash = @{ password = $Password }
+		if ( $Roles )		{ $bodyHash.roles		= @( $Roles );	Write-Verbose "Roles added to request body" }
+		if ( $FullName )	{ $bodyHash.'full_name'	= $FullName;	Write-Verbose "FullName added to request body" }
+		if ( $Email )		{ $bodyHash.email		= $Email;		Write-Verbose "Email added to request body" }
+		if ( $Metadata )	{ $bodyHash.metadata	= $Metadata;	Write-Verbose "Metadata added to request body" }
+		# convert the hashtable to JSON
+		$body = $bodyHash | ConvertTo-JSON -Depth 100 -Compress
+		Write-Verbose "Request Body: $body"
+	}
+	Process {
+		$result = Invoke-RestMethod -Method Post -Body $body -ContentType "application/json" -Uri $uri -Credential $Credential
+		if ( $result.user.created ) {
+			Write-Verbose "User $Username created."
+		} else {
+			Write-Verbose "User $Username updated."
+		}
+		Write-Output $result
+	}
+}
