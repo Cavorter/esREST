caf991f4032be4a2fb4a33e16a504ca3a3c61f18
Nathan Stohlmann <nathan@virtualmicropolis.com>
2016-11-10 08:17:19 -0600

    New function: New-ESIndexPrivilegeGroup. Helper function for creating esREST.IndexPrivilegeGroup objects.

---
+++ Functions/New-ESIndexPrivilegeGroup.Tests.ps1
@@ -0,0 +1,61 @@
+﻿$functionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Split('.')[0]
+. "$PSScriptRoot\$functionName.ps1"
+
+Set-Alias -Name Test-Function -Value $functionName -Scope Script
+
+Describe "$functionName" {
+	$singleIndexVal = "index1"
+	$multiIndexVal = "index1","index2","index3"
+	
+	$singlePrivVal = "all"
+	$multiPrivVal = "manage","index","delete_index"
+	
+	$singleFieldVal = "field1"
+	$multiFieldVal = "field1","field2","field3"
+	
+	It "throws if the Index parameter is null or not specified" {
+		{ Test-Function -Privilege $singlePrivVal -Index } | Should Throw
+	}
+	
+	It "accepts a single value for the index parameter" {
+		( Test-Function -Index $singleIndexVal -Privilege $singlePrivVal ).Index | Should Be $singleIndexVal
+	}
+	
+	It "accepts multiple values for the Index parameter" {
+		Compare-Object -ReferenceObject ( Test-Function -Index $multiIndexVal -Privilege $singlePrivVal ).Index -DifferenceObject $multiIndexVal | Should BeNullOrEmpty
+	}
+	
+	It "throws if the Privilege parameter is null or not specified" {
+		{ Test-Function -Index $singleIndexVal -Privilege } | Should Throw
+	}
+	
+	It "accepts a single value for the Privilege parameter" {
+		( Test-Function -Index $singleIndexVal -Privilege $singlePrivVal ).Privilege | Should Be $singlePrivVal
+	}
+	
+	It "accepts multiple values for the Privilege parameter" {
+		Compare-Object -ReferenceObject ( Test-Function -Index $singleIndexVal -Privilege $multiPrivVal ).Privilege -DifferenceObject $multiPrivVal | Should BeNullOrEmpty
+	}
+	
+	It "only accepts values from 'esIndexPrivilege' module variable" {
+		{ Test-Function -Index $singleIndexVal -Privilege "NotAPrivilege" } | Should Throw
+	}
+	
+	foreach ( $priv in $esIndexPrivilege ) {
+		It "accepts esIndexPrivilege value of $priv" {
+			( Test-Function -Index $singleIndexVal -Privilege $priv ).Privilege | Should Be $priv
+		}
+	}
+	
+	It "accepts a single value for the Field parameter" {
+		( Test-Function -Index $singleIndexVal -Privilege $singlePrivVal -Field $singleFieldVal ).Field | Should Be $singleFieldVal
+	}
+	
+	It "accepts multiple values for the Field parameter" {
+		Compare-Object -ReferenceObject ( Test-Function -Index $singleIndexVal -Privilege $singlePrivVal -Field $multiFieldVal ).Field -DifferenceObject $multiFieldVal | Should BeNullOrEmpty
+	}
+	
+	It "returns an object of type esREST.IndexPrivilegeGroup" {
+		Test-Function -Index $singleIndexVal -Privilege $singlePrivVal | Should BeOfType esREST.IndexPrivilegeGroup
+	}
+}
---
+++ Functions/New-ESIndexPrivilegeGroup.ps1
@@ -0,0 +1,66 @@
+﻿function New-ESIndexPrivilegeGroup {
+	<#
+		.SYNOPSIS
+			Generates a new IndexPrivilegeGroup object used with Set-ESRole
+		.DESCRIPTION
+			The Set-ESRole function allows for one or more specific sets of privileges to be set for indices in an ElasticSearch cluster. This function helps generate objects to populate the Role parameter for Set-ESRole.
+			
+			This function does not yet support the query/document property.
+		.PARAMETER Index
+			One or more index these privileges will apply to in the ElasticSearch cluster.
+		.PARAMETER Privilege
+			The name of one or more privileges to set for the specified indices.
+		.PARAMETER Field
+			The name of one or more fields to restrict the scope of the role in the specified indices.
+		.EXAMPLE
+			C:\PS> $privGroup = New-ESIndexPrivilegeGroup -Index "foo","bar" -Privilege read
+			C:\PS> Set-ESRole -Role foobar_read -IndexPrivilegeGroup $privGroup -BaseURI http://some.escluster.url:12345 -Credential (Get-Credential)
+			
+			While creating a new role that grants the Read privilege to the foo and bar indices we first generate an IndexPrivilegeGroup with those specifications and then pass the resulting object to Set-ESRole.
+		.EXAMPLE
+			C:\PS> $baseUri = "http://some.escluster.url:12345"; $esCreds = Get-Credential
+			C:\PS> $existingRole = Get-ESRole -Role morecomplicated -BaseURI $baseUri -Credential $esCreds
+			C:\PS> $existingRole.PrivilegeGroup += New-ESIndexPrivilegeGroup -Index someindex -Privilege all
+			C:\PS> $existingRole.PrivilegeGroup += New-ESIndexPrivilegeGroup -Index yetanotherindex -Privilege read -Field "source","message"
+			C:\PS> Set-ESRole -Role $existingRole -BaseURI $baseUri -Credential $esCreds
+			
+			Reads an existing role called "morecomplicated" and then adds two new IndexPrivilegeGroups to the role object before updating the role on the cluster.
+		.LINK
+			https://www.elastic.co/guide/en/shield/current/configuring-rbac.html
+	#>
+	Param (
+		[Parameter(Mandatory=$true)]
+		[string[]]$Index,
+		
+		[string[]]$Field
+		
+		<# TODO - Implement query/document property
+		[hashtable]$Query
+		#>
+	)
+    DynamicParam {
+		# Generate the Privilege parameter
+		$RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+		
+		$ParameterName = 'Privilege'
+		$AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+		$ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+		$ParameterAttribute.Mandatory = $Mandatory
+		$ParameterAttribute.Position = 1
+		$AttributeCollection.Add($ParameterAttribute)
+
+		# Set the ValidateSet
+		$ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute( [enum]::GetValues( [type]'esREST.IndexPrivilege') )
+		$AttributeCollection.Add($ValidateSetAttribute)
+		
+		$RuntimeParameterDictionary.Add( $ParameterName, ( New-Object System.Management.Automation.RuntimeDefinedParameter( $ParameterName, [string[]], $AttributeCollection ) ) )
+		return $RuntimeParameterDictionary
+    }
+	Begin {
+		$Privilege = $PsBoundParameters['Privilege']
+	}
+	Process {
+		$result = New-Object -TypeName esRest.IndexPrivilegeGroup -Property @{ Index = $Index; Privilege = $Privilege; Field = $Field }
+		Write-Output $result
+	}
+}
