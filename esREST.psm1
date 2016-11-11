b89007354a525c55466ec9585ef4da864041243c
Nathan Stohlmann <nathan@virtualmicropolis.com>
2016-11-10 18:22:17 -0600

    Import type definitions during module loading and export functions explicitly

--- esREST.psm1
+++ esREST.psm1
@@ -1,4 +1,12 @@
+# Load custom types
+Add-Type -TypeDefinition (Get-Content $PSScriptRoot\esREST-Types.cs | Out-String)
+
+$exportFunctions = @()
 # Load Functions
 foreach ( $function in (Get-ChildItem -Path $PSScriptRoot\Functions\*.ps1 -Exclude *.Tests.*) ) {
 	. $function.FullName
+	$exportFunctions += $function.Name.Split('.')[0]
 }
+Write-Verbose "Exported functions: $exportFunctions"
+
+Export-ModuleMember -Function $exportFunctions
