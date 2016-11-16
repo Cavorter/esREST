336d197be400bfa309586756aa3e6dfb9b50bd81
Nathan Stohlmann <nathan@virtualmicropolis.com>
2016-11-16 10:52:45 -0600

    New function: Get-ESIndex. Retrieves settings for a specified index or indices

---
+++ Functions/Get-ESIndex.ps1
@@ -0,0 +1,36 @@
+﻿function Get-ESIndex {
+	<#
+		.SYNOPSIS
+			Retrieves the settings for an index from an ElasticSearch cluster.
+		.DESCRIPTION
+			Reads the _settings, _mappings, _warmers and _aliases features for the specified index or indices from an ElasticSearch cluster. Implements the GET method for the Index endpoint of the REST API.
+		.PARAMETER Name
+			The name of the index or indices to retrieve settings for. Accepts wildcard values.
+		.LINKS
+			https://www.elastic.co/guide/en/elasticsearch/reference/2.3/indices-get-index.html
+	#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true)]
+		[string]$Name,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$BaseURI,
+		
+		[Parameter(Mandatory=$true)]
+		[pscredential]$Credential
+	)
+	
+	begin {
+		if ( $BaseURI[-1] -eq '/' ) { $BaseURI = $BaseURI.Substring( 0 , $BaseURI.Length - 1 ) }
+		[uri]$uri = "$BaseURI/$Name"
+		Write-Verbose "Uri: $($uri.AbsoluteUri)"
+	}
+	
+	process {
+		$result = Invoke-RestMethod -UseBasicParsing -Credential $Credential -Method Delete -Uri $uri
+		$resultText = $result | ConvertTo-Json -Depth 100 -Compress
+		Write-Verbose "Result: $resultText"
+		Write-Output $result
+	}
+}
