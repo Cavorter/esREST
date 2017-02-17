09391d9338c9ba13c189efb3b3372ee72017e51a
Nathan Stohlmann <nathan@virtualmicropolis.com>
2017-02-17 11:14:12 -0600

    Added function Remove-ESRole

---
+++ Functions/Remove-ESRole.Tests.ps1
@@ -0,0 +1,59 @@
+﻿$functionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Split('.')[0]
+. "$PSScriptRoot\$functionName.ps1"
+
+Set-Alias -Name Test-Function -Value $functionName -Scope Script
+
+Describe "$functionName" {
+	Mock -CommandName Invoke-RestMethod -MockWith { return $true }
+	
+	$goodUri = "http://some.esserver.com:12345"
+	$goodCreds = [pscredential]::new( "SomeUser" , ( ConvertTo-SecureString -String "SomePassword" -AsPlainText -Force ) )
+	$commonParams = @{ BaseURI = $goodUri; Credential = $goodCreds }
+	
+	$goodRole = "somerole"
+	$badRole = "notarole"
+	$goodParams = @{ Role = $goodRole }
+	
+    Context "Standard parameter tests" {
+		It "throws if the BaseURI parameter is null or not specified" {
+			{ Test-Function @goodParams -Credential $goodCreds -BaseURI } | Should Throw
+		}
+		
+		It "throws if the Credential parameter is null or not specified" {
+			{ Test-Function @goodParams -BaseURI $goodUri -Credential } | Should Throw
+		}
+		
+		It "passes the value for the BaseURI parameter" {
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { $Uri -like "$goodUri/*" } }
+			Mock @mockParams -MockWith { return $true }
+			Test-Function @goodParams @commonParams
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+		
+		It "passes the value for the Credential parameter" {
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { $Credential -eq $goodCreds } }
+			Mock @mockParams -MockWith { return $true }
+			Test-Function @goodParams @commonParams
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+	}
+	
+	Context "Parameter Tests" {
+		It "throws if the Role parameter is null or empty" {
+			{ Test-Function @commonParams -Role } | Should Throw
+		}
+		
+		It "passes the value of the Role parameter properly" {
+			$mockParams = @{ CommandName = "Invoke-RestMethod"; ParameterFilter = { $Uri -like "*/_shield/role/$goodRole" } }
+			Mock @mockParams -MockWith { return $true }
+			Test-Function @goodParams @commonParams
+			Assert-MockCalled @mockParams -Exactly 1 -Scope It
+		}
+		
+		It "returns $true if the role was deleted" -Pending {
+			$resultVal = '{ "found" : true }' #| ConvertFrom-Json
+			Mock -CommandName Invoke-RestMethod -MockWith { Write-Output $resultVal }
+			Test-Function @goodParams @commonParams -Verbose | Should Be $true
+		}
+	}
+}
---
+++ Functions/Remove-ESRole.ps1
@@ -0,0 +1,46 @@
+﻿function Remove-ESRole {
+	<#
+		.SYNOPSIS
+			Removes a role from an ElasticSearch cluster
+		.DESCRIPTION
+			Deletes a role in the "native" realm in an ElasticSearch cluster.
+			Wraps DELETE method of REST API at /_shield/role/<rolename>
+		.PARAMETER Role
+			The account to delete.
+		.PARAMETER BaseURI
+			The URI for an ElasticSearch server. If not specified uses the value of $env:esBaseURI
+		.PARAMETER Credential
+			User account for specified ElasticSearch server. If not specified users the value of $esCredential
+		.LINKS
+			https://www.elastic.co/guide/en/shield/current/shield-rest.html
+	#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true)]
+		[string]$Role,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$BaseURI,
+		
+		[Parameter(Mandatory=$true)]
+		[pscredential]$Credential
+	)
+	
+	begin {
+		if ( $BaseURI[-1] -eq '/' ) { $BaseURI = $BaseURI.Substring( 0 , $BaseURI.Length - 1 ) }
+		[uri]$uri = "$BaseURI/_shield/role/$Role"
+		Write-Verbose "Uri: $($uri.AbsoluteUri)"
+	}
+	
+	process {
+		$result = Invoke-RestMethod -UseBasicParsing -Credential $Credential -Method Delete -Uri $uri
+		$resultText = $result | ConvertTo-Json -Depth 100 -Compress
+		Write-Verbose "Result: $resultText"
+		if ( $result.found ) {
+			Write-Verbose "Role $Role deleted."
+		} else {
+			Write-Verbose "Role $Role not found."
+		}
+		Write-Output $result.found
+	}
+}
