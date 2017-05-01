c383c33a1537c49e3cc870dd7d3d55792b398f81
Nathan Stohlmann <nathan.stohlmann@verifybrand.com>
2017-05-01 18:01:15 -0500

    New function: Remove-ESIndex. Removes an index.

---
+++ Functions/Remove-ESIndex.Tests.ps1
@@ -0,0 +1,49 @@
+﻿$functionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Split('.')[0]
+. "$PSScriptRoot\$functionName.ps1"
+
+Set-Alias -Name Test-Function -Value $functionName -Scope Script
+
+Describe "$functionName" {
+	Mock -CommandName Invoke-RestMethod -MockWith { return $true }
+	
+	$goodUri = "http://some.esserver.com:12345"
+	$goodCreds = [pscredential]::new( "SomeUser" , ( ConvertTo-SecureString -String "SomePassword" -AsPlainText -Force ) )
+	$commonParams = @{ BaseURI = $goodUri; Credential = $goodCreds }
+	
+	$nameVal = "someIndex"
+	$goodParams = @{ Name = $nameVal }
+	
+	Mock -CommandName Invoke-RestMethod -MockWith { return $true }
+	
+	Context "Standard Parameter Tests" {
+		$mandatoryKeys = @()
+		$mandatoryKeys += $goodParams.Keys
+		$mandatoryKeys += $commonParams.Keys
+		foreach ( $mandatory in $mandatoryKeys ) {
+			It "the Mandatory attribute for the $mandatory parameter is $true" {
+				( Get-Command -Name $functionName ).Parameters."$mandatory".ParameterSets.__AllParameterSets.IsMandatory | Should Be $true
+			}
+		}
+		
+		$testResult = Test-Function @goodParams @commonParams
+		It "passes the value for the BaseURI parameter" {
+			Assert-MockCalled -Exactly 1 -Scope Context -CommandName Invoke-RestMethod -ParameterFilter { $Uri -like "$goodUri*" }
+		}
+		
+		It "passes the value for the Credential parameter" {
+			Assert-MockCalled -Exactly 1 -Scope Context -CommandName Invoke-RestMethod -ParameterFilter { $Credential -eq $goodCreds }
+		}
+	}
+	
+	Context "Happy Path" {
+		$testResult = Test-Function @goodParams @commonParams
+		
+		It "passes the value of the Name parameter" {
+			Assert-MockCalled -Exactly 1 -Scope Context -CommandName Invoke-RestMethod -ParameterFilter { $Uri -like "*/$nameVal" }
+		}
+		
+		It "specifies the Delete method" {
+			Assert-MockCalled -Exactly 1 -Scope Context -CommandName Invoke-RestMethod -ParameterFilter { $Method -eq "Delete" }
+		}
+	}
+}
---
+++ Functions/Remove-ESIndex.ps1
@@ -0,0 +1,38 @@
+﻿function Remove-ESIndex {
+	<#
+		.SYNOPSIS
+			Removes an index from the cluster
+		.DESCRIPTION
+			Removes an index from the cluster
+		.PARAMETER Name
+			The name of the index to remove
+		.PARAMETER BaseURI
+			The URI of the ElasticSearch server to interact with
+		.PARAMETER Credential
+			PSCredential object for the specified ElasticSearch cluster
+	#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true)]
+		[string]$Name,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$BaseURI,
+		
+		[Parameter(Mandatory=$true)]
+		[pscredential]$Credential
+	)
+	
+	begin {
+		if ( $BaseURI[-1] -eq '/' ) { $BaseURI = $BaseURI.Substring( 0 , $BaseURI.Length - 1 ) }
+		[uri]$uri = "$BaseURI/$Name"
+		Write-Verbose "Uri: $($uri.AbsoluteUri)"
+	}
+	
+	process {
+		$result = Invoke-RestMethod -UseBasicParsing -Credential $Credential -Uri $uri -Method Delete
+		$resultText = $result | ConvertTo-Json -Depth 100 -Compress
+		Write-Verbose "Result: $resultText"
+		Write-Output $result
+	}
+}
