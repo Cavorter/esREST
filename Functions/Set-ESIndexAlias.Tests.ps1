bdd9c60ff3e9586b65a54a8be47a86877a7f62dd
Nathan Stohlmann <nathan.stohlmann@verifybrand.com>
2017-05-24 08:03:21 -0500

    New function: Set-ESIndexAlias. Sets an alias on an index.

---
+++ Functions/Set-ESIndexAlias.Tests.ps1
@@ -0,0 +1,51 @@
+﻿$functionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Split('.')[0]
+. "$PSScriptRoot\$functionName.ps1"
+
+Set-Alias -Name Test-Function -Value $functionName -Scope Script
+
+Describe "$functionName" {
+	
+	$goodUri = "http://some.esserver.com:12345"
+	$goodCreds = [pscredential]::new( "SomeUser" , ( ConvertTo-SecureString -String "SomePassword" -AsPlainText -Force ) )
+	
+	$indexVal = "someIndex"
+	$goodAlias = "someAlias"
+	$goodParams = @{ BaseURI = $goodUri; Credential = $goodCreds; Index = $indexVal; Alias = $goodAlias }
+	
+	Mock -CommandName Invoke-RestMethod -MockWith { return $true }
+	
+	Context "Standard Parameter Tests" {
+		foreach ( $mandatory in $goodParams.Keys ) {
+			It "the Mandatory attribute for the $mandatory parameter is $true" {
+				( Get-Command -Name $functionName ).Parameters."$mandatory".ParameterSets.__AllParameterSets.IsMandatory | Should Be $true
+			}
+		}
+		
+		$testResult = Test-Function @goodParams
+		$assertParams = @{ CommandName = "Invoke-RestMethod"; Times = 1; Exactly = [switch]$true; Scope = "Context" }
+		It "Passes the BaseURI to Invoke-RestMethod" {
+			Assert-MockCalled @assertParams -ParameterFilter { $Uri -like "$goodUri*" }
+		}
+		
+		It "Passes the Credential to Invoke-RestMethod" {
+			Assert-MockCalled @assertParams -ParameterFilter { $Credential.UserName -eq $goodCreds.UserName }
+		}
+	}
+	
+	Context "Happy Path" {
+		$testResult = Test-Function @goodParams
+		
+		$assertParams = @{ CommandName = "Invoke-RestMethod"; Times = 1; Exactly = [switch]$true; Scope = "Context" }
+		It "Passes the Index to Invoke-RestMethod" {
+			Assert-MockCalled @assertParams -ParameterFilter { $Uri -like "*/$indexVal/_alias/*" }
+		}
+		
+		It "Passes the Alias to Invoke-RestMethod" {
+			Assert-MockCalled @assertParams -ParameterFilter { $Uri -like "*/_alias/$goodAlias" }
+		}
+		
+		It "Specifies the PUT method for Invoke-RestMethod" {
+			Assert-MockCalled @assertParams -ParameterFilter { $Method -eq "Put" }
+		}
+	}
+}
---
+++ Functions/Set-ESIndexAlias.ps1
@@ -0,0 +1,45 @@
+﻿function Set-ESIndexAlias {
+	<#
+		.SYNOPSIS
+			Creates an alias for an index
+		.DESCRIPTION
+			Adds an alias to an index
+		.PARAMETER Index
+			The name of the index. Wildcards are not permitted.
+		.PARAMETER Alias
+			The name of the alias to add.
+		.PARAMETER BaseURI
+			The URI for a server in an ElasticSearch cluster
+		.PARAMETER Credential
+			Credential object for the specified cluster
+		.LINKS
+			https://www.elastic.co/guide/en/elasticsearch/reference/2.3/indices-aliases.html
+	#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true)]
+		[string]$Index,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$Alias,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$BaseURI,
+		
+		[Parameter(Mandatory=$true)]
+		[pscredential]$Credential
+	)
+	
+	begin {
+		if ( $BaseURI[-1] -eq '/' ) { $BaseURI = $BaseURI.Substring( 0 , $BaseURI.Length - 1 ) }
+		[uri]$uri = "$BaseURI/$Index/_alias/$Alias"
+		Write-Verbose "Uri: $($uri.AbsoluteUri)"
+	}
+	
+	process {
+		$result = Invoke-RestMethod -UseBasicParsing -Credential $Credential -Uri $uri -Method Put
+		$resultText = $result | ConvertTo-Json -Depth 100 -Compress
+		Write-Verbose "Result: $resultText"
+		Write-Output $result
+	}
+}
